602042d31fc2b01ebd5c37b13988c5cbbf86dd4f
jwshin <singee98@naver.com>
2023-12-29 12:12:43 +0900

    16장 블로그형식

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import styled from "styled-components";
+//page
+import MainPage from "./component/page/MainPage";
+import PostWritePage from "./component/page/PostWritePage";
+import PostViewPage from "./component/page/PostViewPage";
+
+const MainTitleText = styled.p`
+font-size: 22px;
+font-weight: bold;
+text-align: center;
+`;
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <MainTitleText>💗지원쨩의 미니보드💗</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="post-write" element={<PostWritePage />} />
+        <Route path="post/:postId" element={<PostViewPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+
+export default App;
